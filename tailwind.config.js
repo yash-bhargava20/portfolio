fc29abfaade3b0963f4d0d6d64064f7c3e0287d9
yash-bhargava20 <bhargavayash94@gmail.com>
2024-12-04 21:50:21 +0530

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "Sans-Serif"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        insetBlack: "inset 0 0 8px 4px rgba( 0,0,0,0.4)",
+      },
+      keyframes: {
+        borderRadius: {
+          "0%": { borderRadius: "60% 40% 30% 70%/60% 30% 70% 40%" },
+          "50%": { borderRadius: "40% 60% 70% 40%/50% 60% 40% 70%" },
+          "100%": { borderRadius: "60% 40% 30% 70%/60% 30% 70% 40%" },
+        },
+      },
+      animation: {
+        borderRadius: "borderRadius 9s ease-in-out infinite 1s",
+      },
+    },
+  },
+  plugins: [],
+};
